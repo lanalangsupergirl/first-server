07d7167d4e656450712eb1f025a47bbea2b80071
lanalang <lana.lang.supergirl@gmail.com>
2022-11-02 03:08:00 +0700

    images folder added

--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
+//const fs = require("fs").promises;
 const dataRecipes = require("./recipes.json");
 
 const host = "localhost";
@@ -6,13 +9,35 @@ const port = 8080;
 const recipes = JSON.stringify(dataRecipes);
 
 const recipesListener = function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
-  if (req.url === "/recipes") {
-    res.end(recipes);
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ error: "Resource not found" }));
+
+  const parsedReq = url.parse(req.url, true).query;
+  console.log("parsedReq", parsedReq);
+
+  switch (req.url) {
+    case "/recipes":
+      res.end(recipes);
+      break;
+    case `/recipe?id=${parsedReq.id}`:
+      let recipe = dataRecipes.recipes.find(
+        (recipe) => recipe.id === parsedReq.id
+      );
+      console.log("recipe", recipe);
+      res.end(JSON.stringify(recipe));
+      break;
+    case "/images/img1_1.png":
+      const data = fs.readFileSync(__dirname + req.url);
+      res.setHeader("Content-Type", "image/png");
+      res.end(data);
+      // fs.readFile(__dirname + req.url).then(contents => {
+      //   res.setHeader("Content-Type", "image/png");
+      //   res.end(contents);
+      // })
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: "Resource not found" }));
   }
 };
 
