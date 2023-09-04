aa7477eeb431d20e7f06cf0f014d39c1370c5a66
Vladimir Shvets <stormherz@gmail.com>
2023-09-05 01:17:29 +0700

    fix requests for addUser

--- add_user.js
+++ add_user.js
@@ -1,6 +1,7 @@
 import { db, errorHandler } from "./utils.js";
 
 export async function addUser(user) {
+  // TODO: а для чего именно тут два промиса? что делает один и что второй
   return new Promise((resolve, reject) => {
     new Promise((res, rej) => {
       db.all("SELECT login FROM authentication", [], (err, rows) => {
@@ -10,6 +11,7 @@ export async function addUser(user) {
         res(rows);
       });
     }).then((data) => {
+      // TODO: убрать
       if (data === []) {
         new Promise(() => {
           db.run(
@@ -21,6 +23,12 @@ export async function addUser(user) {
         });
       }
 
+      // TODO: сейчас сделано так, что ты ВСЮ таблицу забираешь в переменную и в коде сервера вот тут
+      // проверяешь нет ли там пользователя. теперь представь, что у тебя в этой таблице миллион записей
+      // и у тебя сотня пользователей решила добавить себя на сайте в один и тот же момент
+      // что сделает твой скрипт? верно, вытащит миллион записей в оперативную память 100 раз одновременно
+      // поздравляю, ты уронила сервер
+      // эту проверку надо переписать на SELECT запрос, который проверит, есть ли записи с таким логином в таблице
       let exists = data.some((el) => {
         return el.login === user.login;
       });
@@ -29,6 +37,8 @@ export async function addUser(user) {
 
       if (exists === true) {
         reject(new Error("false"));
+        // TODO: почему бы не сделать тут return, сразу завершив выполнение, а код, идущий далее в
+        // else просто вынести из else?
       } else {
         new Promise(() => {
           db.run(
@@ -40,5 +50,7 @@ export async function addUser(user) {
         });
       }
     });
-  }).catch((err) => err.message);
+  }).catch((err) => err.message); // TODO: вот что конкретно тут должно происходить? мы получаем
+  // ошибку а потом просто опрашиваем ее сообщение, в пустоту. никуда не выводим, ничего не делаем
+  // с ним, просто его берем в никуда
 }
