0b0120666b8c6e5d7168ce764661ede410398598
lanalang <lana.lang.supergirl@gmail.com>
2023-09-06 03:58:31 +0700

    code refactoring

--- add_user.js
+++ add_user.js
@@ -1,56 +1,33 @@
 import { db, errorHandler } from "./utils.js";
 
 export async function addUser(user) {
-  // TODO: а для чего именно тут два промиса? что делает один и что второй
   return new Promise((resolve, reject) => {
-    new Promise((res, rej) => {
-      db.all("SELECT login FROM authentication", [], (err, rows) => {
+    db.get(
+      "SELECT login FROM authentication WHERE login = ?",
+      [user.login],
+      (err, row) => {
         if (err) {
-          rej(err);
+          errorHandler;
         }
-        res(rows);
-      });
-    }).then((data) => {
-      // TODO: убрать
-      if (data === []) {
-        new Promise(() => {
-          db.run(
-            "INSERT INTO authentication(login, password) VALUES (?, ? )",
-            [user.login, user.password],
-            errorHandler
-          );
-          resolve("true");
-        });
+        resolve(row);
+      }
+    );
+  })
+    .then((data) => {
+      if (data) {
+        return "false";
       }
 
-      // TODO: сейчас сделано так, что ты ВСЮ таблицу забираешь в переменную и в коде сервера вот тут
-      // проверяешь нет ли там пользователя. теперь представь, что у тебя в этой таблице миллион записей
-      // и у тебя сотня пользователей решила добавить себя на сайте в один и тот же момент
-      // что сделает твой скрипт? верно, вытащит миллион записей в оперативную память 100 раз одновременно
-      // поздравляю, ты уронила сервер
-      // эту проверку надо переписать на SELECT запрос, который проверит, есть ли записи с таким логином в таблице
-      let exists = data.some((el) => {
-        return el.login === user.login;
+      let result = new Promise((res, rej) => {
+        db.run(
+          "INSERT INTO authentication(login, password) VALUES (?, ? )",
+          [user.login, user.password],
+          errorHandler
+        );
+        res("true");
       });
 
-      console.log("exists", exists);
-
-      if (exists === true) {
-        reject(new Error("false"));
-        // TODO: почему бы не сделать тут return, сразу завершив выполнение, а код, идущий далее в
-        // else просто вынести из else?
-      } else {
-        new Promise(() => {
-          db.run(
-            "INSERT INTO authentication(login, password) VALUES (?, ? )",
-            [user.login, user.password],
-            errorHandler
-          );
-          resolve("true");
-        });
-      }
-    });
-  }).catch((err) => err.message); // TODO: вот что конкретно тут должно происходить? мы получаем
-  // ошибку а потом просто опрашиваем ее сообщение, в пустоту. никуда не выводим, ничего не делаем
-  // с ним, просто его берем в никуда
+      return result;
+    })
+    .catch(errorHandler);
 }
